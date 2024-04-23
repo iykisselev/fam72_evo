720a515200b6293a6292dcfb6630d56acce21032
iykisselev <145861034+iykisselev@users.noreply.github.com>
2024-04-22 23:17:43 -0400

    Create haplotype_freq_plt.R

---
+++ chapter_3.2/haplotype_freq_plt.R
@@ -0,0 +1,68 @@
+# The script calculates and plots frequencies of FAM72A-D haplotypes 
+# Load necessary libraries
+library(vcfR)
+library(dplyr)
+library(ggplot2)
+library(rcartocolor)
+library(ggpubr)
+library(pegas)
+library(stringr)
+
+# Read population data and genetic coordinates for FAM72 genes
+popdata <- read.delim("20130606_g1k_3202_samples_ped_population.txt", sep = " ")
+fam72_grch38 <- read.table("fam72.grch38.tsv", header = FALSE)
+
+# Function to extract haplotypes and generate a bar plot for a given gene region
+haplotype_bar <- function(vcf_file, gene, popdata) {
+  # Read the VCF file
+  v <- read.vcfR(vcf_file)
+  
+  # Convert to DNAbin and extract haplotypes with a minimum frequency of 50
+  v_dnabin <- vcfR2DNAbin(v, extract.indels = FALSE)
+  haps_50 <- haplotype(v_dnabin) %>% subset(minfreq = 50) %>% sort()
+  
+  # Pre-allocate a list to store data frames
+  df_list <- lapply(seq_along(attr(haps_50, "index")), function(i) {
+    data.frame(
+      Haplotype = rep(attr(haps_50, "dimnames")[[1]][i], 
+                      length(attr(haps_50, "index")[[i]])),
+      Sample = attr(v_dnabin, "dimnames")[[1]][attr(haps_50, "index")[[i]]]
+    )
+  })
+  
+  # Combine all data frames into one
+  df <- do.call(rbind, df_list)
+  
+  # Join with population data and count the number of samples per haplotype and superpopulation
+  df_hist <- df %>% mutate(Sample = str_remove_all(Sample, "_.+")) %>%
+    left_join(popdata[,c(2,6,7)], by = c("Sample" = "SampleID")) %>%
+    group_by(Haplotype, Superpopulation) %>%
+    summarise(n = n(), .groups = 'drop')
+  df_hist$Haplotype <- factor(df_hist$Haplotype, levels = 
+           (df_hist %>% group_by(Haplotype) %>% summarise(f = sum(n)) %>% 
+              arrange(desc(f)))$Haplotype)
+  
+  # Plot the haplotype distribution
+  plt <- ggplot(data = df_hist, aes(y = n, x = Haplotype, fill = Superpopulation)) + 
+    geom_bar(position = "stack", stat = "identity") + theme_bw() + 
+    scale_fill_manual(values = carto_pal(6, "Bold")[-6]) + ggtitle(gene) + theme(
+      legend.text = element_text(size=14), legend.title = element_text(size=14),
+      axis.title = element_text(size=12)) + ylab("Haplotype count")
+  
+  return(plt)
+}
+
+# Generate bar plots for each FAM72 gene based on its genomic region
+plots <- lapply(1:nrow(fam72_grch38), function(i) {
+  gene_info <- fam72_grch38[i, ]
+  vcf_filename <- paste0(gene_info$V4, ".1KG.vcf.gz") 
+  haplotype_bar(vcf_filename, toupper(gene_info$V4), popdata)
+})
+
+# Arrange the haplotype plots in a grid
+haplotypes_plot <- ggarrange(plotlist = plots, ncol = 2, nrow = 2, common.legend = TRUE, legend = "right")
+
+# Save the plot
+svg("./pop_diff/hap_freq.svg", width = 11.3, height = 10)
+print(haplotypes_plot)
+dev.off()
